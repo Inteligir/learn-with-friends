a29d62ee0fa6c3ca95ebaa3fc5f5255cfb4eb69c
Kaiyu Hsu <im.kaiyu@gmail.com>
2015-10-19 00:52:23 -0700

    Added copy

---
+++ app/data/en.js
@@ -0,0 +1,29 @@
+export default {
+  messages: {
+    header: {
+      'posts': 'Feed',
+      'guides': 'Groups',
+      'protected': 'Protected'
+    },
+    groups: {
+      'page-title': 'Groups'
+    },
+    protected: {
+      'page-title': 'Protected Page'
+    },
+    post: {
+      'page-title': 'Post - {title}',
+      'not-found-page-title': 'Post not found'
+    },
+    posts: {
+      'page-title': 'Feed'
+    },
+    routes: {
+      'posts': '/feed',
+      'post': '/post/:id',
+      'groups': '/groups',
+      'protected': '/protected',
+      'login-info': '/login-info'
+    }
+  }
+};
